04f4786146878586dfc3a210d59af590c890bcf6
Aziz TAS <tasaziz4106@gmail.com>
2024-09-24 17:20:38 +0200

    something changed

--- library-service/src/main/java/com/kitaplik/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/LibraryServiceApplication.java
@@ -1,13 +1,12 @@
 package com.kitaplik.libraryservice;
 
-import org.springframework.context.annotation.Bean;
 import com.kitaplik.libraryservice.client.RetreiveMessageErrorDecoder;
 import feign.Logger;
 import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,12 +15,10 @@ public class LibraryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
-
 /*
-
 	//Feign Client Error Handling
 	@Bean
-	public ErrorDecoder errorDecoder(){
+	public ErrorDecoder errorDecoder() {
 		return new RetreiveMessageErrorDecoder();
 	}
 
@@ -30,5 +27,4 @@ public class LibraryServiceApplication {
 		return Logger.Level.FULL;
 	}
 */
-
 }
