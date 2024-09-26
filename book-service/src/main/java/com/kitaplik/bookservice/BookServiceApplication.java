1eea887ecf72a9c41b8ed64eea8a35dcce845332
Aziz TAS <tasaziz4106@gmail.com>
2024-09-26 22:34:11 +0200

    Basic gRPC communication is completed

--- book-service/src/main/java/com/kitaplik/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/kitaplik/bookservice/BookServiceApplication.java
@@ -2,13 +2,18 @@ package com.kitaplik.bookservice;
 
 import com.kitaplik.bookservice.model.Book;
 import com.kitaplik.bookservice.repository.BookRepository;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -20,7 +25,8 @@ public class BookServiceApplication implements CommandLineRunner {
 		this.repository = repository;
 	}
 
-	public static void main(String[] args) {SpringApplication.run(BookServiceApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
 	}
 
 	@Override
@@ -34,4 +40,13 @@ public class BookServiceApplication implements CommandLineRunner {
 		System.out.println(bookList);
 	}
 
+	@Bean
+	public GrpcServerConfigurer keepAliveServerConfigurer() {
+		return serverBuilder -> {
+			if (serverBuilder instanceof NettyServerBuilder) {
+				((NettyServerBuilder) serverBuilder).keepAliveTime(30, TimeUnit.SECONDS)
+						.keepAliveTimeout(5, TimeUnit.SECONDS).permitKeepAliveWithoutCalls(true);
+			}
+		};
+	}
 }
--- library-service/src/main/java/com/kitaplik/libraryservice/service/LibraryService.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/service/LibraryService.java
@@ -1,26 +1,31 @@
 package com.kitaplik.libraryservice.service;
 
+import com.kitaplik.bookservice.dto.BookId;
+import com.kitaplik.bookservice.dto.BookServiceGrpc;
+import com.kitaplik.bookservice.dto.Isbn;
 import com.kitaplik.libraryservice.client.BookServiceClient;
 import com.kitaplik.libraryservice.dto.AddBookRequest;
 import com.kitaplik.libraryservice.dto.LibraryDto;
 import com.kitaplik.libraryservice.exception.LibraryNotFoundException;
 import com.kitaplik.libraryservice.model.Library;
 import com.kitaplik.libraryservice.repository.LibraryRepository;
-
-import org.springframework.http.ResponseEntity;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class LibraryService {
+
     private final LibraryRepository libraryRepository;
     private final BookServiceClient bookServiceClient;
 
+    @GrpcClient("book-service")
+    private BookServiceGrpc.BookServiceBlockingStub bookServiceBlockingStub;
 
-    public LibraryService(LibraryRepository libraryRepository, BookServiceClient bookServiceClient) {
+    public LibraryService(LibraryRepository libraryRepository,
+                          BookServiceClient bookServiceClient) {
         this.libraryRepository = libraryRepository;
         this.bookServiceClient = bookServiceClient;
     }
@@ -28,23 +33,23 @@ public class LibraryService {
     public LibraryDto getAllBooksInLibraryById(String id) {
         Library library = libraryRepository.findById(id)
                 .orElseThrow(() -> new LibraryNotFoundException("Library could not found by id: " + id));
+
         LibraryDto libraryDto = new LibraryDto(library.getId(),
                 library.getUserBook()
                         .stream()
-                        .map(book -> bookServiceClient.getBookById(book).getBody()) //feign
+                        .map(book -> bookServiceClient.getBookById(book).getBody())
                         .collect(Collectors.toList()));
         return libraryDto;
-
     }
 
     public LibraryDto createLibrary() {
         Library newLibrary = libraryRepository.save(new Library());
-
         return new LibraryDto(newLibrary.getId());
     }
 
     public void addBookToLibrary(AddBookRequest request) {
-        String bookId = bookServiceClient.getBookByIsbn(request.getIsbn()).getBody().getBookId();
+        BookId bookIdByIsbn = bookServiceBlockingStub.getBookIdByIsbn(Isbn.newBuilder().setIsbn(request.getIsbn()).build());
+        String bookId = bookIdByIsbn.getBookId();
 
         Library library = libraryRepository.findById(request.getId())
                 .orElseThrow(() -> new LibraryNotFoundException("Library could not found by id: " + request.getId()));
@@ -53,8 +58,8 @@ public class LibraryService {
                 .add(bookId);
 
         libraryRepository.save(library);
-
     }
+
 
     public List<String> getAllLibraries() {
 
